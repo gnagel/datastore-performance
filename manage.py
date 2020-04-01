63c5ca73064bb2266351da6d5767cab0082ad38c
Glenn Nagel <glenn@mercury-wireless.com>
2020-04-01 15:05:46 -0400

    Adding remote app-engine support

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import
 
+import os
+import time
+import urllib2
+
 import click
 from jinja2 import Template
 
@@ -39,14 +43,50 @@ def benchmark_serialization():
 
 @generate.command()
 def benchmark_crud():
-    setUpModule()
+    application_id = os.environ.get('APPLICATION_ID', None)
+    # Initialize either the mock stubs, or a remote connection to app engine
+    if not application_id:
+        setUpModule()
+    else:
+        _boot_app_engine(application_id)
+
     results = benchmark_crud_models()
-    tearDownModule()
+
+    if not application_id:
+        tearDownModule()
 
     csv_string = format_csv(results)
 
     with open('benchmark_crud.csv', 'w') as file_handle:
         file_handle.write(csv_string)
+
+
+def _boot_app_engine(project_id):
+    #
+    # Boot the App Engine environment
+    #
+
+    # This makes the GCS work!
+    # http://stackoverflow.com/questions/22646641/google-cloud-storage-client-with-remote-api-on-local-client
+    os.environ["SERVER_SOFTWARE"] = ""
+    os.environ["HTTP_HOST"] = project_id + '.appspot.com'
+
+    # Always use UTC here so entities timestamps get updated with UTC
+    os.environ['TZ'] = 'UTC'
+    time.tzset()
+
+    from google.appengine.ext.remote_api import remote_api_stub
+
+    try:
+        remote_api_stub.ConfigureRemoteApiForOAuth(os.environ["HTTP_HOST"], '/_ah/remote_api')
+    except urllib2.HTTPError as e:
+        click.echo("ConfigureRemoteApiForOAuth failure ({})".format(e))
+        click.echo('')
+        click.echo("To authenticate run:")
+        click.echo("    gcloud auth application-default login")
+        return
+
+    os.environ['SERVER_SOFTWARE'] = 'remote_api_shell'
 
 
 if __name__ == "__main__":
